972f76984889297006050012421dbfc2af8b14f7
Gui Costa <gui.costalp@gmail.com>
2022-11-24 11:28:09 +1300

    Item 4 Done. Add book button set

--- main.js
+++ main.js
@@ -36,11 +36,111 @@ let myLibrary = [
 
 ];
 
+
+//Create new Object on array - Insert object (input) on Array
+
+function createBook(){
+
+    
+    myLibrary.push(getInput());
+
+    createDiv(getInput().title, getInput().author, getInput().pages, getInput().read);
+
+}
+
+//Loop through the Array - Load page
+
+for (i=0; i < myLibrary.length; i++) {
+
+    createDiv(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].read);
+
+}
+
+
+                // FUNCTIONS
+
+
+//Get form inputs - Transforms in object (input)                
+
+function getInput(){
+
+    const formTitle = document.querySelector('#title').value;
+    const formAuthor = document.querySelector('#author').value;
+    const formPages = document.querySelector('#pages').value;
+    const formRead = document.querySelector('#read').value;
+
+    const input = {
+
+        title: formTitle,
+        author: formAuthor,
+        pages: formPages,
+        read: formRead
+
+    }
+
+    return input;
+
+}
+
+
+
+
+//Create DIV for each element on array
+
+function createDiv (title, author, pages, read) {
+
+    const div = document.createElement("div"); //create tag
+    div.setAttribute("id", "book"); //set ID
+    div.setAttribute("class", "books"); //set CLASS
+
+    //Create DIV Content
+    let createText = '';
+
+    createText += '<p>Title: '+title+'</p>';
+    createText += '<p>Author: '+author+'</p>';
+    createText += '<p>Pages: '+pages+'</p>';
+    createText += '<p>Read?: '+read+'</p>';
+
+
+    //Insert content inside DIV
+    div.innerHTML = createText;
+
+    //appendChield to div ID books
+    document.getElementById("books").appendChild(div);
+    
+}
+
+//Use Constructor Book to create newBook input
+
+function addBook(title, author, pages, read){
+
+
+    const newBook = new Book(title, author, pages, read)
+
+    //Push newBook to the Array
+    myLibrary.push(newBook);
+
+}
+
+//Constructor Book
+
+function Book (title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function (){
+
+        return title +" by "+ author +", " + pages+", " + read
+
+    }
+
+}
+
 //Form visibility
 
 const btn = document.getElementById('show-form');
 
-console.log(btn);
 
 btn.addEventListener('click', () => {
     const form = document.getElementById('main-form');
@@ -57,69 +157,6 @@ btn.addEventListener('click', () => {
 
 })
 
-//Loop through the Array
-
-for (i=0; i < myLibrary.length; i++) {
-
-    createDiv();
-
-}
-
-
-function createDiv () {
-
-    const div = document.createElement("div"); //create tag
-    div.setAttribute("id", "book"); //set ID
-    div.setAttribute("class", "books"); //set CLASS
-
-    //Create DIV Content
-    let createText = '';
-
-    createText += '<p>Title: '+myLibrary[i].title+'</p>';
-    createText += '<p>Author: '+myLibrary[i].author+'</p>';
-    createText += '<p>Pages: '+myLibrary[i].pages+'</p>';
-    createText += '<p>Read?: '+myLibrary[i].read+'</p>';
-
-
-    //Insert content inside DIV
-    div.innerHTML = createText;
-
-    //appendChield to div ID books
-    document.getElementById("books").appendChild(div);
-    
-}
-
-
-
-
-
-function addBook(){
-
-    //Use Constructor Book to create newBook input
-    const newBook = new Book("abc", "xyz", 258, false)
-
-    //Push newBook to the Array
-    myLibrary.push(newBook);
-
-}
-
-
-
-
-//Constructor
-
-function Book (title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function (){
-
-        return title +" by "+ author +", " + pages+", " + read
-
-    }
-
-}
 
 
 
