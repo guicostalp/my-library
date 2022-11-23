04563193be82e9b885245cdb2d0f32fc3c9b2b22
Gui Costa <gui.costalp@gmail.com>
2022-11-24 12:09:56 +1300

    Index number implemented for delete button and unique entry

--- main.js
+++ main.js
@@ -3,35 +3,40 @@ let myLibrary = [
         title: "The Hobbit" ,
         author: "Tolkien" ,
         pages: 1258 ,
-        read: true
+        read: true,
+        index: 0
     },
 
     {
         title: "Alice in Wonderland" ,
         author: "Lewis Carol" ,
         pages: 359 ,
-        read: true
+        read: true,
+        index: 1
     },
 
     {
         title: "Game of Thrones" ,
         author: "George Martin" ,
         pages: 978 ,
-        read: false
+        read: false,
+        index: 2
     },
 
     {
         title: "The Archer" ,
         author: "Bernard Cornwell" ,
         pages: 453 ,
-        read: true
+        read: true,
+        index: 3
     },
 
     {
         title: "Excalibur" ,
         author: "Bernard Cornwell" ,
         pages: 689 ,
-        read: true
+        read: true,
+        index: 4
     }
 
 ];
@@ -43,8 +48,8 @@ function createBook(){
 
     
     myLibrary.push(getInput());
-
-    createDiv(getInput().title, getInput().author, getInput().pages, getInput().read);
+    console.log(myLibrary)
+    createDiv(getInput().title, getInput().author, getInput().pages, getInput().read, getInput().index);
 
 }
 
@@ -52,7 +57,9 @@ function createBook(){
 
 for (i=0; i < myLibrary.length; i++) {
 
-    createDiv(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].read);
+
+    index = myLibrary.indexOf(i);
+    createDiv(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].read, myLibrary[i].index);
 
 }
 
@@ -69,14 +76,19 @@ function getInput(){
     const formPages = document.querySelector('#pages').value;
     const formRead = document.querySelector('#read').value;
 
+    const indexInput = myLibrary.length - 1;
+
+
     const input = {
 
         title: formTitle,
         author: formAuthor,
         pages: formPages,
-        read: formRead
+        read: formRead,
+        index: indexInput
 
     }
+
 
     return input;
 
@@ -87,11 +99,13 @@ function getInput(){
 
 //Create DIV for each element on array
 
-function createDiv (title, author, pages, read) {
+function createDiv (title, author, pages, read, index) {
 
     const div = document.createElement("div"); //create tag
     div.setAttribute("id", "book"); //set ID
     div.setAttribute("class", "books"); //set CLASS
+
+
 
     //Create DIV Content
     let createText = '';
@@ -100,7 +114,8 @@ function createDiv (title, author, pages, read) {
     createText += '<p>Author: '+author+'</p>';
     createText += '<p>Pages: '+pages+'</p>';
     createText += '<p>Read?: '+read+'</p>';
-
+    createText += '<p>Index #: '+index+'</p>';
+    createText += '<button class="deleteBtn">Delete</button>'
 
     //Insert content inside DIV
     div.innerHTML = createText;
